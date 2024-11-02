af4f1ae18a2c1f756b3565678b63cff4e76085bc
Hector Robertson <hectormrobertson@gmail.com>
2024-11-02 13:57:53 +0000

    Changing README.md and __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,8 @@
-import json, requests
+import json, webbrowser
 
 def GET(name, tld):
     with open("connections.json") as file:
         o = json.load(file)
         for website in o:
             if website == f"{name}.{tld}":
-                page = requests.get(o[website]["source"])
-                return page.text
+                webbrowser.open(o[website]["source"])
